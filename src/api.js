0f03c3512b01ac181dc53359405bff87309628b8
Adrian Kerber <adrianmathiaskerber@gmail.com>
2025-05-25 22:21:53 -0400

    test

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://todo-backend-nwuf.onrender.com";
 
 // Dias
 export const getDias = () => fetch(`${BASE_URL}/dias`).then(res => res.json());
